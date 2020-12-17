666328408c3cbe1abe8dbde9b983b949e59e3390
AmaruEscalante <sebastian.escalante@utec.edu.pe>
2020-12-17 03:07:15 -0500

    Added Dockerfile

--- lab06_ds/settings.py
+++ lab06_ds/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'lab06_ds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_poo',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
