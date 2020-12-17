2adfbc1489b69463527a7c6941f8e2bec74f7281
Ubuntu <ubuntu@ip-172-31-10-189.sa-east-1.compute.internal>
2020-12-17 16:09:41 +0000

    Added dockerfiel

--- lab06_ds/settings.py
+++ lab06_ds/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'lab06_ds.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_poo',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
